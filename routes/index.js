c6153e3c6a323c358c523dd43fb8d6034bb85f04
Aaron C. de Bruyn <aaron@heyaaron.com>
2020-11-04 16:53:56 -0800

    Rewrite processAllUsers to be cleaner and more efficient

--- routes/index.js
+++ routes/index.js
@@ -15,16 +15,28 @@ router.get("/listallclasses", listallclasses);
 module.exports = router;
 
 async function processAllUsers(req, res) {
-  let stuff = await database.getUsers();
-  for (let i = 0; i < stuff.length; i++) {
-    const next = stuff[i];
-    let lastNames = await database.getLastNames();
-    const lastNameLookup = createLastNameLookup(lastNames);
-    const nextId = next.id;
-    const last = lastNameLookup[nextId];
-    next.last = last;
-  }
-  res.json(stuff);
+  // For the purposes of the example, I'll assume there's a reason the database has a split between
+  // first name and last name fields and the two results have to be joined in Node--I can't just pull from the database
+  // in a more efficient manner like:
+  // SELECT id, name, last FROM users;
+  // ...which I think is probably the point of the exercise.
+
+  // Grab users and last names
+  let users = await database.getUsers();
+  let lastNames = await database.getLastNames();
+
+  // Create a new array called 'results' by mapping the existing users array
+  var results = users.map(function (user) {
+    // use Object.assign to get a dictionary merged with results from lastNames based on the ID field
+    // This assumes every user and lastName has a corresponding ID
+    return Object.assign(user, lastNames.find(function(lastName) {
+      return user.id === lastName.id
+    }))
+  });
+
+  // Return the newly built array of user dictionaries containing an ID, a name, and a last field.
+  // I'm assuming there are clients dependent on the API, so we can't
+  res.json(results);
 }
 
 function createLastNameLookup(array) {
